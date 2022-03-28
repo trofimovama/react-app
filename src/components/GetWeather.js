6560ab91913cac9371c7c3076b31f2b6fbb9b6e6
Maria Trofimova <maria.trofimova@oxagile.com>
2022-03-28 14:41:55 +0300

    add function to get weather data

--- src/components/GetWeather.js
+++ src/components/GetWeather.js
@@ -6,15 +6,18 @@ const GetWeather = () => {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
 
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=31437cba04d38b2ed41b44a1882df5f7&lang=ru`;
+  const getData = (cityName) => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=31437cba04d38b2ed41b44a1882df5f7&lang=ru`;
+    axios.get(url).then((response) => {
+      setData(response.data);
+      console.log(response.data);
+    });
+    setLocation("");
+  };
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
-      axios.get(url).then((response) => {
-        setData(response.data);
-        console.log(response.data);
-      });
-      setLocation("");
+      getData(location);
     }
   };
 
